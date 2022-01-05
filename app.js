8b56e07603880f59472f8b6bbee66e2258a20028
bysch <1259364843@qq.com>
2022-01-05 13:05:52 +0800

    页面注册

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 App({
+  // 生命周期函数
   onLaunch() {
+    console.log('小程序初始化完成');
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -13,7 +15,19 @@ App({
       }
     })
   },
+  // 界面显示出来
+  onShow: function (options) {
+    console.log('界面显示出来',options);
+  },
+  // 界面隐藏时
+  onHide: function () {
+    console.log('界面隐藏');
+  },
+  // 发生错误时
+  onError: function (err) {
+    
+  },
   globalData: {
     userInfo: null
-  }
+  }  
 })
